caedd05213c1733f3fc7d5b6e69968fbcd70ee2b
Efeglas <gabor.atty@gmail.com>
2023-03-18 22:04:07 +0100

    Added modal definitions
    - test description

--- database/database.js
+++ database/database.js
@@ -6,7 +6,8 @@ const UserModel = require('./models/user-model.js');
 const PasswordModel = require('./models/password-model.js');
 const RoleModel = require('./models/role-model.js');
 const PermissionModel = require('./models/permission-model.js');
-const RolePermissionModel = require('./models/role-permission-model.js');;
+const RolePermissionModel = require('./models/role-permission-model.js');
+const RefreshTokenModel = require('./models/reftoken-model.js');
 
 class Database {
     constructor() {
@@ -38,38 +39,44 @@ class Database {
         try {
             await this.sequelize.authenticate();
             console.log('Connection has been established successfully.');
+        
+            //? force <-> alter
+            UserModel.init(this.sequelize);
+            //!await UserModel.sync({ alter: true });
+    
+            PasswordModel.init(this.sequelize);
+            //!await PasswordModel.sync({ alter: true });
+    
+            RoleModel.init(this.sequelize);
+            //!await RoleModel.sync({ alter: true });
+    
+            PermissionModel.init(this.sequelize);
+            //!await PermissionModel.sync({ alter: true });
+           
+            RolePermissionModel.init(this.sequelize);
+            //!await RolePermissionModel.sync({ alter: true });
+
+            RefreshTokenModel.init(this.sequelize);
+    
+            UserModel.belongsTo(RoleModel);
+            RoleModel.hasMany(UserModel);
+    
+            PasswordModel.belongsTo(UserModel);
+            UserModel.hasMany(PasswordModel);
+
+            RefreshTokenModel.belongsTo(UserModel);
+            UserModel.hasMany(RefreshTokenModel);
+    
+            RoleModel.belongsToMany(PermissionModel, { through: RolePermissionModel });
+            PermissionModel.belongsToMany(RoleModel, { through: RolePermissionModel });
+    
+            //!this.sequelize.sync({ alter: true });
+                
+            this.models = {UserModel, PasswordModel, RoleModel, PermissionModel, RolePermissionModel};
+
         } catch (error) {
             console.error('Unable to connect to the database:', error.parent.sqlMessage);
         }
-
-        //? force <-> alter
-        UserModel.init(this.sequelize);
-        await UserModel.sync({ force: true });
-
-        PasswordModel.init(this.sequelize);
-        await PasswordModel.sync({ force: true });
-
-        RoleModel.init(this.sequelize);
-        await RoleModel.sync({ force: true });
-
-        PermissionModel.init(this.sequelize);
-        await PermissionModel.sync({ force: true });
-       
-        RolePermissionModel.init(this.sequelize);
-        await RolePermissionModel.sync({ force: true });
-
-        UserModel.belongsTo(RoleModel);
-        RoleModel.hasMany(UserModel);
-
-        PasswordModel.belongsTo(UserModel);
-        UserModel.hasMany(PasswordModel);
-
-        RoleModel.belongsToMany(PermissionModel, { through: RolePermissionModel });
-        PermissionModel.belongsToMany(RoleModel, { through: RolePermissionModel });
-
-        //this.sequelize.sync({ alter: true });
-            
-        this.models = {UserModel, PasswordModel, RoleModel, PermissionModel};
     }
 }
 
