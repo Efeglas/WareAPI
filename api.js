41f9c92d57ef620f5f2ddf9ed078e3b55350037e
Efeglas <gabor.atty@gmail.com>
2023-03-18 22:37:32 +0100

    Token route
    - Added token-routes.js
    - Added route definitions to api.js

--- api.js
+++ api.js
@@ -3,6 +3,7 @@ const express = require('express');
 
 const userRoutes = require('./routes/user-routes');
 const orderRoutes = require('./routes/order-routes');
+const tokenRoutes = require('./routes/token-routes');
 
 const app = express();
 
@@ -16,6 +17,7 @@ app.use((req, res, next) => {
 
 app.use('/user', userRoutes);
 app.use('/order', orderRoutes);
+app.use('/token', tokenRoutes);
 
 app.use((error, req, res, next) => {
   const status = error.status || 500;
---
+++ routes/token-routes.js
@@ -0,0 +1,16 @@
+const Database = require('../database/database.js');
+const express = require('express');
+const jwToken = require('jsonwebtoken');
+const { config } = require('../config/config.js');
+const router = express.Router();
+const bcrypt = require('bcrypt');
+
+router.post('/', async (req, res, next) => {
+    const data = req.body;
+
+    res.json({ data: data.refreshToken });
+});
+
+
+
+module.exports = router;
