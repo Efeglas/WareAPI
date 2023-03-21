e1dc7a7a2b558b6dd16e2963dedf5b4591c6b1e7
Efeglas <gabor.atty@gmail.com>
2023-03-21 21:24:06 +0100

    Sequalize time zone fixes

--- routes/token-routes.js
+++ routes/token-routes.js
@@ -15,37 +15,43 @@ router.post('/', async (req, res, next) => {
     let resultUser = await Database.models.UserModel.findOne({
         attributes: ['id', 'username'], include: [{model: Database.models.RoleModel, attributes: ['id', 'name']}], where: {username: data.username}
     });
-    const planResultUser = resultUser.get({ plain: true });
 
-    const validRefToken = await Database.models.RefreshTokenModel.findOne({where: {UserId: planResultUser.id, token: data.refreshToken, valid: {[Op.gt]: getCorrectedDate(1)}}});
-
-    if (validRefToken === null) {
-
-        res.status(404).json({ message: "Resource not found" });
+    if (resultUser !== null) {
+        
+        const planResultUser = resultUser.get({ plain: true });
+    
+        const validRefToken = await Database.models.RefreshTokenModel.findOne({where: {UserId: planResultUser.id, token: data.refreshToken, valid: {[Op.gt]: getCorrectedDate(1)}}});
+    
+        if (validRefToken === null) {
+    
+            res.status(404).json({ message: "Resource not found" });
+        } else {
+    
+            let token = jwToken.sign({
+                id: planResultUser.id,
+                username: planResultUser.username,
+                role: planResultUser.Role.id,
+                roleName: planResultUser.Role.name            
+              }, config.jwtKey, { expiresIn: `${config.jwtTokenExpMinutes}m` });
+        
+              const refToken = generateRefreshToken();
+    
+              let refreshTokenValidDate = new Date();         
+              refreshTokenValidDate.setDate(refreshTokenValidDate.getDate() + config.refreshTokenValidDay);
+        
+              let invalidateDate = new Date();
+              invalidateDate.setSeconds(invalidateDate.getSeconds() - 5);
+    
+              let tokenExpireTime = new Date();
+              tokenExpireTime.setMinutes(tokenExpireTime.getMinutes() + config.jwtTokenExpMinutes);
+        
+              const updatedRefTokens = await Database.models.RefreshTokenModel.update({valid: invalidateDate}, {where: {UserId: planResultUser.id, valid: {[Op.gt]: getCorrectedDate(1)}}});
+              const savedRefToken = await Database.models.RefreshTokenModel.create({ token: refToken, valid: refreshTokenValidDate, UserId: planResultUser.id });
+    
+              res.json({ message: "New token provided", data: {token: token, refreshToken: refToken, tokenExpire: tokenExpireTime.getTime()} });
+        }
     } else {
-
-        let token = jwToken.sign({
-            id: planResultUser.id,
-            username: planResultUser.username,
-            role: planResultUser.Role.id,
-            roleName: planResultUser.Role.name            
-          }, config.jwtKey, { expiresIn: `${config.jwtTokenExpMinutes}m` });
-    
-          const refToken = generateRefreshToken();
-
-          let refreshTokenValidDate = new Date();         
-          refreshTokenValidDate.setDate(refreshTokenValidDate.getDate() + config.refreshTokenValidDay);
-    
-          let invalidateDate = new Date();
-          invalidateDate.setSeconds(invalidateDate.getSeconds() - 5);
-
-          let tokenExpireTime = new Date();
-          tokenExpireTime.setMinutes(tokenExpireTime.getMinutes() + config.jwtTokenExpMinutes);
-    
-          const updatedRefTokens = await Database.models.RefreshTokenModel.update({valid: invalidateDate}, {where: {UserId: planResultUser.id, valid: {[Op.gt]: getCorrectedDate(1)}}});
-          const savedRefToken = await Database.models.RefreshTokenModel.create({ token: refToken, valid: refreshTokenValidDate, UserId: planResultUser.id });
-
-          res.json({ message: "New token provided", data: {token: token, refreshToken: refToken, tokenExpire: tokenExpireTime.getTime()} });
+        res.status(404).json({ message: "Resource not found" });
     }
 });
 
