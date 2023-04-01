ec2afbcc3acd4c11f05cc008fdc5234af8145dae
Efeglas <gabor.atty@gmail.com>
2023-04-01 21:03:45 +0200

    Adjusted routes with price field
    Added price filed to get route
    Added price field to edit and add routes

--- routes/item-routes.js
+++ routes/item-routes.js
@@ -13,7 +13,14 @@ const {
 
 router.post('/get', autenticated, accessRightItem, accessRightOrder, hasAccess, async (req, res, next) => {
 
-    const result = await Database.models.ItemModel.findAll({attributes: ['id', 'name', 'barcode'], where: {visible: 1}, include: [{model: Database.models.MeasureModel, attributes: ['id', 'name']}]});
+    const result = await Database.models.ItemModel.findAll({
+        attributes: ['id', 'name', 'barcode'], 
+        where: {visible: 1}, 
+        include: [
+            {model: Database.models.MeasureModel, attributes: ['id', 'name']},
+            {model: Database.models.PriceModel, attributes: ['id', 'price'], where: {visible: 1}},
+        ]
+    });
     res.json({ message: "Items accessed", data: result});
 })
 
@@ -26,7 +33,8 @@ router.post('/measure', autenticated, accessRightItem, hasAccess, async (req, re
 router.post('/add', autenticated, accessRightItem, hasAccess, async (req, res, next) => {
 
     const data = req.body;
-    const item = await Database.models.ItemModel.create({name: data.name, barcode: data.barcode, MeasureId: data.measure});
+    const item = await Database.models.ItemModel.create({name: data.name, barcode: data.barcode, MeasureId: data.measure});    
+    const price = await Database.models.PriceModel.create({price: data.price, ItemId: item.id});
     res.json({ message: "Items added"});
 })
 
@@ -34,6 +42,8 @@ router.patch('/edit', autenticated, accessRightItem, hasAccess, async (req, res,
 
     const data = req.body;
     const item = await Database.models.ItemModel.update({name: data.name, barcode: data.barcode, MeasureId: data.measure}, {where: {id: data.id}});
+    const updateOldPrice = await Database.models.PriceModel.update({visible: 0}, {where: {ItemId: data.id, visible: 1}});
+    const price = await Database.models.PriceModel.create({price: data.price, ItemId: data.id});
     res.json({ message: "Items edited"});
 })
 
