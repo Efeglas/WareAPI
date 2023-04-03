dfce3457fe7a71b464922734b50c82d3ed6fe53e
Efeglas <gabor.atty@gmail.com>
2023-04-03 22:08:30 +0200

    Moved generateUsername function from user routes to utility.js

--- utility/utility.js
+++ utility/utility.js
@@ -29,15 +29,45 @@ module.exports.generateTemporaryPass = () => {
 module.exports.toNormalForm = (str) => {
   return str.normalize("NFD").replace(/[\u0300-\u036f]/g, "");
 }
+const toNormalFormHere = module.exports.toNormalForm;
 
 module.exports.getRandomInteger = (min, max) => {
   return Math.floor(Math.random() * (max - min + 1)) + min;
 }
-
 const getRandomIntegerHere = module.exports.getRandomInteger;
+
 module.exports.generate2Char = () => {    
   let letter1 = String.fromCharCode(getRandomIntegerHere(97,122));
   let letter2 = String.fromCharCode(getRandomIntegerHere(97,122));     
   let result = letter1 + letter2;
   return result;
+}
+const generate2CharHere = module.exports.generate2Char;
+
+module.exports.generateUsername = async (lastName, database) => {
+
+  let number = 1;     
+  let userBegining = toNormalFormHere(lastName.substring(0, 2));
+  let randomChars = generate2CharHere();
+  let userFix = "hrt";
+  
+  let userExists = null;
+
+  do {
+    
+    let builtUsername = `${userBegining}${randomChars}${number}${userFix}`;
+
+    userExists = await database.models.UserModel.findOne({where: {username: builtUsername}});
+
+    if (userExists === null) {         
+      return builtUsername.toLowerCase();       
+    } else {
+      number++;
+      if (number > 9) {
+        number = 1;
+        randomChars = generate2CharHere();
+      }
+    }
+  } while (userExists !== null);
+
 }
